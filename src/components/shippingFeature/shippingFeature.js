63c7134305a950a82b6f792d3956d02a37208f5a
itsalivenger <alihoussa16@gmail.com>
2025-10-30 14:35:13 +0100

    finished icons i assume

--- src/components/shippingFeature/shippingFeature.js
+++ src/components/shippingFeature/shippingFeature.js
@@ -5,22 +5,22 @@ import LazyMedia from '../lazyMedia/LazyMedia';
 function ShippingFeatures() {
     const features = [
         {
-            icon: <LazyMedia type={'image'} src='/images/icons/truck.svg' className={styles.icon} alt='truck' />,
+            icon: <LazyMedia type={'image'} src='/images/icons/delivery.png' className={styles.icon} alt='truck' />,
             title: 'Livraison Gratuite',
             description: 'Bénéficiez de la livraison gratuite pour toute commande supérieure à 50 DH.'
         },
         {
-            icon: <LazyMedia type={'image'} src='/images/icons/package.svg' className={styles.icon} alt='package' />,
+            icon: <LazyMedia type={'image'} src='/images/icons/delivery-status.png' className={styles.icon} alt='package' />,
             title: 'Retours Gratuits',
             description: 'Retournez vos produits gratuitement dans les 30 jours suivant votre achat.'
         },
         {
-            icon: <LazyMedia type={'image'} src='/images/icons/clock.svg' className={styles.icon} alt='clock' />,
+            icon: <LazyMedia type={'image'} src='/images/icons/express-delivery.png' className={styles.icon} alt='clock' />,
             title: 'Livraison Express',
             description: 'Recevez votre commande en 24 à 48 heures, selon votre localisation.'
         },        
         {
-            icon: <LazyMedia type={'image'} src='/images/icons/pod.svg' className={styles.icon} alt='shield' />,
+            icon: <LazyMedia type={'image'} src='/images/icons/payment-protection.png' className={styles.icon} alt='shield' />,
             title: 'Paiement Sécurisé',
             description: 'Optez pour un paiement sécurisé par Cash on Delivery lors de la réception de votre commande.'
         }
